4271dfb27ce741d1be877d64b8d596e4ba2d84df
MasahideHayashi <imgaoler@icloud.com>
2025-09-24 17:28:51 +0900

    youtube posedata added

--- video2numpylist.py
+++ video2numpylist.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 
 #img_path = '../tests/data/coco/000000000785.jpg'   # replace this with your own image path
-video_path = "video/bad"
+video_path = "video/youtube"
 
 files = os.listdir(video_path)
 
@@ -13,7 +13,7 @@ inferencer = MMPoseInferencer('human')
 # The MMPoseInferencer API employs a lazy inference approach,
 # creating a prediction generator when given input
 for file in files:
-    if file[-4:] != ".mp4" : continue
+    if file[-4:] != ".mov" : continue
     result_generator = inferencer(video_path+"/"+file, show=False)
     results = [result for result in result_generator]
     l = []
@@ -21,4 +21,4 @@ for file in files:
         l.append(results[i]["predictions"][0][0]["keypoints"])
     
     l = np.array(l)
-    np.save("posedata/bad/"+file[0:-4], l)
+    np.save("posedata/youtube/"+file[0:-4], l)
